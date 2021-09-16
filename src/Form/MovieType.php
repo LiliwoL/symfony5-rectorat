ecc667efb3b9aa0ca45e4edbdde668f7231a35b3
Nicolas C <github@liliwol.fr>
2021-09-16 09:39:05 +0200

    ok

--- src/Form/MovieType.php
+++ src/Form/MovieType.php
@@ -7,6 +7,7 @@ use App\Entity\Movie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -70,7 +71,16 @@ class MovieType extends AbstractType
                     'required' => false
                 ]
             )
-            
+            ->add(
+                'submit',
+                SubmitType::class,
+                [
+                    'label' => 'Enregistrer',
+                    'attr' => [
+                        'class' => 'btn btn-primary'
+                    ]
+                ],
+            )          
         ;
     }
 
