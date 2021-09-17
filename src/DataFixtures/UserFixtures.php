bae5af0ab2b0691fcc72655f728b15a9187f29b3
Nicolas C <github@liliwol.fr>
2021-09-17 12:25:06 +0200

    ok

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Movie;
+use App\Entity\User;
+use App\Service\GeneratePassword;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+/**
+ * 
+ * Cette classe va nous permettre d'ajouter en base des utilsiateurs lambdas
+ * 
+ */
+class UserFixtures extends Fixture
+{
+    // L'encodeur en attribut
+    private $_encoder;
+
+    // Le constructeur prend l'interface donnantt accès au hashage utilisé pour le smots de passe
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->_encoder = $encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        //Nouvel instance d'utilisateur
+        $user = new User();
+
+        // Utilisateur Simple
+        $user->setUsername('user');
+        
+        // Il faut hasher les mots de passe
+        $password = $this->_encoder->hashPassword( $user, 'user');
+        $user->setPassword($password);
+
+        // Roles
+        $user->setRoles(
+            ['ROLE_USER']
+        );
+
+        // Persister ce user en base
+        $manager->persist($user);
+
+
+        // **************************************
+        
+        $admin = new User();
+        $admin->setUsername("admin");
+
+        // Encodage du mot de passe
+        $password = $this->_encoder->hashPassword($admin, 'admin');
+        $admin->setPassword($password);
+
+        // Roles
+        $admin->setRoles(
+            ['ROLE_ADMIN']
+        );
+
+        $manager->persist($admin);
+
+
+        // Envoi de toutes les modifs à la base
+        $manager->flush();
+    }
+}
