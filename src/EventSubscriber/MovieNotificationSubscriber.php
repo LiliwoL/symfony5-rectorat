c2455473c405dee8f364681bb3dc31580ade73ea
Nicolas C <github@liliwol.fr>
2021-09-17 08:45:03 +0200

    ok

--- src/EventSubscriber/MovieNotificationSubscriber.php
+++ src/EventSubscriber/MovieNotificationSubscriber.php
@@ -5,16 +5,25 @@ namespace App\EventSubscriber;
 use App\Event\MovieCreatedEvent;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\NoRecipient;
+use Symfony\Component\Notifier\Recipient\Recipient;
 
 class MovieNotificationSubscriber implements EventSubscriberInterface
 {
     private $_logger;
+    private $_notifier;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, NotifierInterface $notifier)
     {
         $this->_logger = $logger;
+        $this->_notifier = $notifier;
     }
 
+    /**
+     * Méthode pour inscrire cette classe à des événements
+     */
     public static function getSubscribedEvents()
     {
         return [
@@ -26,6 +35,10 @@ class MovieNotificationSubscriber implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * Méthode lancée à l'événement MovieCreatedEvent::NAME ou movie.created
+     * @param $event
+     */
     public function onMovieCreated ( MovieCreatedEvent $event ) : void
     {
         // Debug
@@ -37,5 +50,17 @@ class MovieNotificationSubscriber implements EventSubscriberInterface
         // Log
         $this->_logger->info("❤❤❤ Movie Created ❤❤❤");
         $this->_logger->info("❤❤❤ Movie Title: " . $movie->getTitle() . " ❤❤❤");
+
+        // ********
+        
+        // Notification
+        $notification = new Notification('Nouveau film ' . $movie->getTitle());
+        $notification->importance(Notification::IMPORTANCE_HIGH);
+
+        // Destinataire Notif
+        $recipient = new Recipient('test@recipient.fr');
+
+        // Appel du composant Notifier        
+        $this->_notifier->send($notification, $recipient);
     }
 }
